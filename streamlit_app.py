3a2fec90299d690cf96a8e712713231ee27a253a
ADIEZANTON <118805377+ADIEZANTON@users.noreply.github.com>
2022-11-22 11:18:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
-streamlit.title('Breakfast Favorites)
-streamlit.header(' 🫐 🥝 🍓Fruit Your Own Fruit Smoothie ')
-streamlit.text(' 🥬 Leafy GreenKale, Spinach')
+streamlit.title('Breakfast Favorites')
+streamlit.header('🫐 🥝 🍓Fruit Your Own Fruit Smoothie')
+streamlit.text('🥬 Leafy GreenKale, Spinach')
 streamlit.text ('🥘 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑 Avocado Toast ')
+streamlit.text('🥑 Avocado Toast ')
 
 import pandas
 my_fruit_list = pandas.read_csv ("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
