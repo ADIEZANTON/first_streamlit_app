8a52b4f7714c0a7b3ef8b52e84245d99968890d7
ADIEZANTON <118805377+ADIEZANTON@users.noreply.github.com>
2022-11-22 11:08:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Mom´s New Healthy Diner')
-streamlit.header('🥣 Breakfast Menu')
+streamlit.header(' 🥝 🍓Fruit Your Own Fruit Smoothie ')
 streamlit.text('🫐 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥘 Shallow Pan of FoodKale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑 Avocado Toast ')
