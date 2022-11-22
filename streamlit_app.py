c7becdaaad9bb9076c81a643157f7742dd375d16
ADIEZANTON <118805377+ADIEZANTON@users.noreply.github.com>
2022-11-22 10:46:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Rocket Smoothie')
+streamlit.title('Hard_bolied Free_range Egg')
