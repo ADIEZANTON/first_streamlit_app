f91079283b6ab3947b5a8e4743abfdcc95c10f01
ADIEZANTON <118805377+ADIEZANTON@users.noreply.github.com>
2022-11-22 10:50:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Rocket Smoothie')
-streamlit.texty('Hard_bolied Free_range Egg')
+streamlit.text('Hard_bolied Free_range Egg')
 
